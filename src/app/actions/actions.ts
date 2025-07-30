2a6d2cf909b6849cf142020ce2cff348334c8887
Alishba Musharraf <162012518+alishbamusharraf@users.noreply.github.com>
2025-07-30 21:29:09 +0500

    Update actions.ts

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,40 +1,42 @@
-import { Item } from "@radix-ui/react-dropdown-menu";
 import { Product } from "../../../types/products";
 
+// Add product to cart
+export const addToCart = (product: Product) => {
+    const cart: Product[] = JSON.parse(localStorage.getItem('cart') || '[]');
 
-export const addToCart = (product : Product) => {
-    const cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
+    const existingProductIndex = cart.findIndex(item => item._id === product._id);
 
-    const existingProductIndex = cart.findIndex(item => item._id === product. _id)
-
-    if(existingProductIndex > -1) {
-        cart[existingProductIndex].quantity += 1
-    }
-    else {
+    if (existingProductIndex > -1) {
+        cart[existingProductIndex].quantity += 1;
+    } else {
         cart.push({
-            ...product, quantity:1
-        })
+            ...product,
+            quantity: 1
+        });
     }
 
-    localStorage.setItem('cart',JSON.stringify(cart))
-}
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
 
-export const RemoveFromCart = (productID : string, quantity: number) => {
-     let cart : Product[] =JSON.parse(localStorage.getItem('cart') || '[]')
-     cart = cart.filter(Item => Item._id !== productID)
-        localStorage.setItem('cart', JSON.stringify(cart))
-}
+// Remove product from cart
+export const RemoveFromCart = (productID: string) => {
+    let cart: Product[] = JSON.parse(localStorage.getItem('cart') || '[]');
+    cart = cart.filter(item => item._id !== productID);
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
 
-export const underCartQuantity = (productID : string, quantity: number) => {
-    const cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
-    const productIndex = cart.findIndex(item => item._id === productID)
+// Update quantity of a product in cart
+export const underCartQuantity = (productID: string, quantity: number) => {
+    const cart: Product[] = JSON.parse(localStorage.getItem('cart') || '[]');
+    const productIndex = cart.findIndex(item => item._id === productID);
 
     if (productIndex > -1) {
-        cart[productIndex].quantity =  quantity
-        localStorage.setItem('cart', JSON.stringify(cart))
-     }
-}
+        cart[productIndex].quantity = quantity;
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+};
 
-export const getCartItems = () : Product[] => {
-    return JSON.parse(localStorage.getItem('cart') || '[]')
-}
+// Get all items from cart
+export const getCartItems = (): Product[] => {
+    return JSON.parse(localStorage.getItem('cart') || '[]');
+};
